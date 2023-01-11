13b8b89e6d962d2c1ad18e6897c14b5663f44253
ateak <ekaterina_timoshkina@inbox.ru>
2023-01-11 15:22:21 +0300

    начало ООП

--- student_project/src/StudentOrderValidator.java
+++ student_project/src/StudentOrderValidator.java
@@ -7,15 +7,16 @@ public class StudentOrderValidator
 
         while(true) {
             StudentOrder so = readStudentOrder();
-            System.out.println("Start");
+           // System.out.println("Start");
             if (so == null) {
                 break;
             }
-            System.out.println("Finish");
+            //System.out.println("Finish");
 
             AnswerCityRegister cityAnswer = checkCityRegister(so);
             if (!cityAnswer.success) {
-                continue;
+                //continue;
+                break;
             }
 
             AnswerWedding wedAnswer = checkWedding(so);
@@ -24,18 +25,24 @@ public class StudentOrderValidator
 
             sendMail(so);
         }
-        System.out.println("Finish2");
+        //System.out.println("Finish2");
     }
     static StudentOrder readStudentOrder() {
         StudentOrder so = new StudentOrder();
         return so;
     }
     static AnswerCityRegister checkCityRegister(StudentOrder so) {
-        System.out.println("CityRegister is running");
-        AnswerCityRegister ans = new AnswerCityRegister();
-        ans.success = false;
-        return ans;
-
+        CityRegisterValidator crv1 = new CityRegisterValidator();
+        crv1.hostName = "Host1";
+        crv1.login = "Login1";
+        crv1.password = "Password1";
+        CityRegisterValidator crv2 = new CityRegisterValidator();
+        crv2.hostName = "Host2";
+        crv2.login = "Login2";
+        crv2.password = "Password2";
+        AnswerCityRegister ans1 = crv1.checkCityRegister(so);
+        AnswerCityRegister ans2 = crv2.checkCityRegister(so);
+        return ans1;
     }
     static AnswerWedding checkWedding(StudentOrder so) {
         System.out.println("Wedding запущен");
@@ -53,6 +60,6 @@ public class StudentOrderValidator
         return ans;
     }
     static void sendMail(StudentOrder so) {
-
+        System.out.println("Почта отправлена");
     }
 }
