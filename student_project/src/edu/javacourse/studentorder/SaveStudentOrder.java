f3c7479cc3d5383b7e811437815fc5e59533c539
ateak <ekaterina_timoshkina@inbox.ru>
2023-01-26 16:49:47 +0300

    SaveStudentOrder upd

--- student_project/src/edu/javacourse/studentorder/SaveStudentOrder.java
+++ student_project/src/edu/javacourse/studentorder/SaveStudentOrder.java
@@ -1,29 +1,29 @@
 package edu.javacourse.studentorder;
 
+import edu.javacourse.studentorder.domain.Adult;
 import edu.javacourse.studentorder.domain.StudentOrder;
 
 public class SaveStudentOrder
 {
     public static void main(String[] args) {
         StudentOrder so = new StudentOrder();
-        so.hFirstName = "Алексей";
-        so.hLastName = "Петров";
-        so.wFirstName = "Галина";
-        so.wLastName = "Петрова";
-
-        StudentOrder so1 = new StudentOrder();
-        so1.hFirstName = "Алексей";
-        so1.hLastName = "Петров";
-        so1.wFirstName = "Галина";
-        so1.wLastName = "Петрова";
 
         long ans = saveStudentOrder(so);
         System.out.println(ans);
     }
     static long saveStudentOrder(StudentOrder studentOrder) {
         long answer = 199;
-        System.out.println("saveStudentOrder: " + studentOrder.hLastName);
+        System.out.println("saveStudentOrder");
 
         return answer;
+    }
+
+    static StudentOrder buildStudentOrder() {
+        StudentOrder so = new StudentOrder();
+        Adult husband = new Adult();
+        husband.setGivenName("Андрей");
+        so.setHusband(husband);
+
+        return so;
     }
 }
